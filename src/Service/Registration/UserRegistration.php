e545c67d3ff78bc910607d25c9f80b9a7ab6b1b7
Eugene Popov <zhenya1995q@gmail.com>
2018-08-05 01:27:49 +0300

    - .idea

---
+++ src/Service/Registration/UserRegistration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eugene
+ * Date: 05.08.18
+ * Time: 0:14
+ */
+
+namespace App\Service\Registration;
+
+
+class UserRegistration
+{
+
+}
---
+++ src/Service/User/UserService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eugene
+ * Date: 05.08.18
+ * Time: 1:16
+ */
+
+namespace App\Service\User;
+
+
+class UserService
+{
+
+}
